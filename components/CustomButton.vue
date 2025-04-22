2474eb3279777c5a3f473feaa09eafd9bb6e86b3
Evan Martin <evan.martiin29@gmail.com>
2025-04-22 12:39:59 +0200

    :sparkles: (Bouton) Fond brush

--- components/CustomButton.vue
+++ components/CustomButton.vue
@@ -5,5 +5,8 @@ defineProps<{
 </script>
 
 <template>
-    <PrismicLink class="inline-block font-slussen p-[12px] bg-forest" :field="link" />
+    <PrismicLink class="inline-block font-slussen p-[12px] relative" :field="link">
+        <img class="absolute top-1/2 -translate-y-1/2 left-0 -z-1 w-full h-[35px] object-fill" src="/assets/images/btn-brush.png" />
+        <span class="z-0 relative">{{ link.text || link.url }}</span>
+    </PrismicLink>
 </template>
