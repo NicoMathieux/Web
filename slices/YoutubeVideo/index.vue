fc7cc036b2226e23e47e4f36413c519535cf7df7
Evan Martin <evan.martiin29@gmail.com>
2025-05-08 14:06:17 +0200

    :sparkles: (Expedition) YoutubeVideo

--- slices/YoutubeVideo/index.vue
+++ slices/YoutubeVideo/index.vue
@@ -1,8 +1,6 @@
 <script setup lang="ts">
 import type { Content } from "@prismicio/client";
 
-// The array passed to `getSliceComponentProps` is purely optional.
-// Consider it as a visual hint for you when templating your slice.
 defineProps(
   getSliceComponentProps<Content.YoutubeVideoSlice>([
     "slice",
@@ -18,7 +16,14 @@ defineProps(
     :data-slice-type="slice.slice_type"
     :data-slice-variation="slice.variation"
   >
-    Placeholder component for youtube_video (variation: {{ slice.variation }})
-    Slices
+    <PrismicEmbed :field="slice.primary.link" class="youtube-video" />
   </section>
 </template>
+
+<style>
+.youtube-video > iframe {
+  width: 100vw;
+  transform: translateX(-60px);
+  height: calc(100vw * 9 / 16);
+}
+</style>
