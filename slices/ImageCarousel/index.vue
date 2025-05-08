59306610f552d10267c732cdb3072f52a8b6cfe5
Evan Martin <evan.martiin29@gmail.com>
2025-05-08 12:35:00 +0200

    :sparkles: (Expedition) ImageCarousel

--- slices/ImageCarousel/index.vue
+++ slices/ImageCarousel/index.vue
@@ -1,24 +1,66 @@
 <script setup lang="ts">
 import type { Content } from "@prismicio/client";
 
-// The array passed to `getSliceComponentProps` is purely optional.
-// Consider it as a visual hint for you when templating your slice.
 defineProps(
-  getSliceComponentProps<Content.ImageCarouselSlice>([
+  getSliceComponentProps<Content.ImageCarouselLinkSlice>([
     "slice",
     "index",
     "slices",
     "context",
   ]),
 );
+
+const carousel = ref(null);
+
+let currentIndex = 0;
+
+const slideEnded = ({ currentSlideIndex }) => {
+  if (currentSlideIndex === currentIndex) return;
+  currentIndex = currentSlideIndex;
+}
+
+const prevSlide = () => {
+  carousel.value?.prev();
+}
+
+const nextSlide = () => {
+  carousel.value?.next();
+}
 </script>
 
 <template>
   <section
     :data-slice-type="slice.slice_type"
     :data-slice-variation="slice.variation"
+    class="relative"
   >
-    Placeholder component for image_carousel (variation: {{ slice.variation }})
-    Slices
+    <Carousel
+      ref="carousel"
+      :wrap-around="true"
+      @slide-end="slideEnded"
+    >
+      <Slide
+        v-for="image in slice.primary.images"
+        :key="image.title"
+      >
+        <CustomImage
+          :image="image.image"
+          :date="image.date"
+          :place="image.place"
+        />
+      </Slide>
+    </Carousel>
+
+    <div class="absolute top-1/2 -translate-y-1/2 flex justify-between w-full px-[24px] z-[1] pointer-events-none">
+      <button @click="prevSlide" class="pointer-events-auto"><img class="h-[48px] rotate-180" src="/assets/images/arrow.png" /></button>
+      <button @click="nextSlide" class="pointer-events-auto"><img class="h-[48px]" src="/assets/images/arrow.png" /></button>
+    </div>
   </section>
 </template>
+
+<style scoped>
+.custom-image {
+  width: 100%;
+  height: 650px;
+}
+</style>
