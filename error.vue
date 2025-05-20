3f67f165973b1f7a7d62c3b0cc65d806cd912544
Evan Martin <evan.martiin29@gmail.com>
2025-05-20 16:47:40 +0200

    :sparkles: (Error) Page v1

--- error.vue
+++ error.vue
@@ -10,9 +10,14 @@ const handleError = () => clearError({ redirect: '/' })
 
 <template>
   <NuxtLayout>
-    <h4>{{ error.statusMessage }}</h4>
-    <h2>{{ error.statusCode }}</h2>
-    <button @click="handleError">Retour à l'accueil</button>
+    <div class="w-full h-[40vh] flex flex-col items-center justify-center mt-[10vh]">
+      <div class="font-slussen">{{ error.statusMessage }}</div>
+      <div class="font-rader text-2xl leading-[90%] mb-[24px]">{{ error.statusCode }}</div>
+      <button @click="handleError" class="inline-block font-slussen p-[12px] relative">
+          <img class="absolute top-1/2 -translate-y-1/2 left-0 -z-1 w-full h-[35px] object-fill" src="/assets/images/btn-brush-white.png" />
+          <div class="z-0 relative text-coal">Accueil</div>
+      </button>
+    </div>
     <MotionBackground />
   </NuxtLayout>
 </template>
