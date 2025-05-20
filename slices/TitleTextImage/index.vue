0dd7931c9da88bf0f313c6c406cfc62bd8428f08
Evan Martin <evan.martiin29@gmail.com>
2025-05-20 15:16:57 +0200

    :sparkles: (Expeditions) TitleTextImage avec carousel

--- slices/TitleTextImage/index.vue
+++ slices/TitleTextImage/index.vue
@@ -1,7 +1,7 @@
 <script setup lang="ts">
 import type { Content } from "@prismicio/client";
 
-defineProps(
+const props = defineProps(
   getSliceComponentProps<Content.TitleTextImageSlice>([
     "slice",
     "index",
@@ -9,6 +9,25 @@ defineProps(
     "context",
   ]),
 );
+
+const withCarousel = ref(props.slice.variation === 'withCarousel');
+
+const carousel = ref(null);
+
+let currentIndex = 0;
+
+const slideEnded = ({ currentSlideIndex }) => {
+  if (currentSlideIndex === currentIndex) return;
+  currentIndex = currentSlideIndex;
+}
+
+const prevSlide = () => {
+  carousel.value?.prev();
+}
+
+const nextSlide = () => {
+  carousel.value?.next();
+}
 </script>
 
 <template>
@@ -26,12 +45,36 @@ defineProps(
       </div>
     </div>
 
-      <div v-if="slice.primary.image" class="grid grid-cols-5">
+      <div class="grid grid-cols-5">
         <div class="col-span-2">
           <div class="font-slussen"><RichText :field="slice.primary.text" /></div>
           <div class="mt-[18px]"><CustomButton :link="slice.primary.link" /></div>
         </div>
-        <CustomImage :image="slice.primary.image" :date="slice.primary.image_date" :place="slice.primary.image_place" class="col-span-2 col-end-6" />
+        <CustomImage v-if="slice.primary.image && !withCarousel" :image="slice.primary.image" :date="slice.primary.image_date" :place="slice.primary.image_place" class="col-span-2 col-end-6" />
+
+        <div v-else-if="withCarousel" class="col-span-2 col-end-6 relative">
+          <Carousel
+            ref="carousel"
+            :wrap-around="true"
+            @slide-end="slideEnded"
+          >
+            <Slide
+              v-for="image in slice.primary.element"
+              :key="image.title"
+            >
+              <CustomImage
+                :image="image.image"
+                :date="image.date"
+                :place="image.place"
+              />
+            </Slide>
+          </Carousel>
+
+          <div class="absolute top-1/2 -translate-y-1/2 flex justify-between w-full px-[24px] z-[1] pointer-events-none">
+            <button @click="prevSlide" class="pointer-events-auto"><img class="h-[48px] rotate-180" src="/assets/images/arrow.png" /></button>
+            <button @click="nextSlide" class="pointer-events-auto"><img class="h-[48px]" src="/assets/images/arrow.png" /></button>
+          </div>
+        </div>
       </div>
   </section>
 </template>
@@ -40,5 +83,6 @@ defineProps(
 <style scoped>
 .custom-image {
   height: 600px;
+  width: 100%;
 }
 </style>
