46d5db0c7c2fdba05d1613e1ccbf1b3d65390cae
Evan Martin <evan.martiin29@gmail.com>
2025-06-10 18:28:08 +0200

    :sparkles: (Env) Middleware prêt

--- middleware/dev-only.ts
+++ middleware/dev-only.ts
@@ -1,20 +1,8 @@
 export default defineNuxtRouteMiddleware((to, from) => {
 	if (process.env.NODE_ENV === "production") {
-		if (import.meta.client) {
-			const error = createError({
-				statusCode: 404,
-				statusMessage: `Page not found: ${to.path}`,
-			});
-            
-			const nuxtApp = useNuxtApp();
-			nuxtApp.callHook('app:error', error);
-            
-			return abortNavigation();
-		} else {
-			throw createError({
-				statusCode: 404,
-				statusMessage: `Page not found: ${to.path}`,
-			});
-		}
+		throw createError({
+			statusCode: 404,
+			statusMessage: `Page not found: ${to.path}`,
+		});
 	}
 });
--- pages/contact.vue
+++ pages/contact.vue
@@ -1,10 +1,6 @@
 <script setup lang="ts">
 import { components } from "~/slices";
 
-definePageMeta({
-	middleware: "dev-only",
-});
-
 const page = getPrismicSingle("contact");
 
 useMeta(page);
