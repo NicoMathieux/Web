2ba40ef2fb75f8b5cede29271554da84263ee247
Evan Martin <evan.martiin29@gmail.com>
2025-06-10 18:19:45 +0200

    :white_check_mark: (Env) Nouveau middleware spa/ssr

--- middleware/dev-only.ts
+++ middleware/dev-only.ts
@@ -1,8 +1,20 @@
 export default defineNuxtRouteMiddleware((to, from) => {
 	if (process.env.NODE_ENV === "production") {
-		throw createError({
-			statusCode: 404,
-			statusMessage: `Page not found: ${to.path}`,
-		});
+		if (import.meta.client) {
+			const error = createError({
+				statusCode: 404,
+				statusMessage: `Page not found: ${to.path}`,
+			});
+            
+			const nuxtApp = useNuxtApp();
+			nuxtApp.callHook('app:error', error);
+            
+			return abortNavigation();
+		} else {
+			throw createError({
+				statusCode: 404,
+				statusMessage: `Page not found: ${to.path}`,
+			});
+		}
 	}
 });
